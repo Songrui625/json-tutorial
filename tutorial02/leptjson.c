433514a99ffc9ad46dbbd3b15a4add19a2887ad2
Songrui625 <1029229159@qq.com>
2021-01-29 12:25:51 +0800

    tutorial2 finished.

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -1,6 +1,10 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
+#include <errno.h>   /* errno, ERANGE */
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
@@ -14,7 +18,7 @@ static void lept_parse_whitespace(lept_context* c) {
         p++;
     c->json = p;
 }
-
+/*
 static int lept_parse_true(lept_context* c, lept_value* v) {
     EXPECT(c, 't');
     if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
@@ -41,23 +45,76 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     v->type = LEPT_NULL;
     return LEPT_PARSE_OK;
 }
+ */
+
+static int lept_parse_literal(lept_context *c, lept_value *v, char *literal, lept_type type) {
+    /* 以下这种做法是错的，因为如果传进来的json是 "null x"，
+     * 那就直接返回了一个LEPT_PARSE_INVALID_VALUE,
+     * 但真正应该返回的是LEPT_PARSE_NOT_SINGULAR.
+    int len = strlen(literal);
+    int i;
+    if (strlen(c->json) != len)
+        return LEPT_PARSE_INVALID_VALUE;
+    for (i = 0; i < len; i++)
+        if (c->json[i] != literal[i])
+            return LEPT_PARSE_INVALID_VALUE;
+    c->json += len;
+     */
+    /* 以下写法也是错的，因为循环的结束条件可能永远成立，字符数组的结束条件永远（保留）
+     * 应该是 p != '\0'
+    int i = 0;
+    for (i = 0; c->json[i] == literal[i]; i++) {
+#if 1
+        printf("c->json[%d]=%c, literal[%d]=%c \n", i, c->json[i], i, literal[i]);
+#endif
+    }
+     */
+    int i;
+    for (i = 0; literal[i]; i++)
+        if (c->json[i] != literal[i])
+            return LEPT_PARSE_INVALID_VALUE;
+    c->json += i;
+    v->type = type;
+    return LEPT_PARSE_OK;
+}
+
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 
 static int lept_parse_number(lept_context* c, lept_value* v) {
-    char* end;
+    const char *p = c->json;
+    if (*p == '-') p++;
+    if (*p == '0') p++;
+    else {
+        if (!ISDIGIT1TO9(*p)) return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    if (*p == '.') {
+        p++;
+        if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    if (*p == 'e' || *p == 'E') {
+        p++;
+        if (*p == '+' || *p == '-') p++;
+        if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    errno = 0;
     /* \TODO validate number */
-    v->n = strtod(c->json, &end);
-    if (c->json == end)
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json = end;
+    v->n = strtod(c->json, NULL);
+    if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+        return LEPT_PARSE_NUMBER_TOO_BIG;
     v->type = LEPT_NUMBER;
+    c->json = p;
     return LEPT_PARSE_OK;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-        case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_literal(c, v, "true", LEPT_TRUE);
+        case 'f':  return lept_parse_literal(c, v, "false", LEPT_FALSE);
+        case 'n':  return lept_parse_literal(c, v, "null", LEPT_NULL);
         default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
